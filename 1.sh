2ee252f9ab34eedaf652d17036392787a068bb02
rabithoy <emailforfrom@gmail.com>
2025-04-10 01:15:37 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -10,27 +10,19 @@ while true; do
   TOKEN=$(echo $RESPONSE | grep -oP '"appToken":\s*"\K([^"]+)')
 
   if [ "$TOKEN" != "" ] && [ "$TOKEN" != "$CURRENT_TOKEN" ]; then
-  #  echo "🔁 Token mới: $TOKEN"
-
-    # 🧨 Xóa container cũ nếu có
-    echo "🧨 Xoá container cũ (nếu tồn tại)..."
+    # Xóa container cũ nếu có
     if docker ps -a --format '{{.Names}}' | grep -q "^$NAME$"; then
       docker stop $NAME >/dev/null 2>&1
       docker rm $NAME >/dev/null 2>&1
-    #  echo "✅ Đã xóa container cũ."
     fi
 
-    # 🚀 Chạy container mới
-   # echo "🚀 Chạy Docker với token: $TOKEN"
+    # Chạy container mới
     docker run -d --name $NAME -e TOKEN="$TOKEN" traffmonetizer/cli_v2 start accept --token "$TOKEN"
 
     # Cập nhật token hiện tại
     CURRENT_TOKEN=$TOKEN
-  else
-   # echo "✅ Token hiện tại vẫn là $CURRENT_TOKEN. Không đổi hoặc không có token mới."
   fi
 
   echo "ilovingyou"
   sleep 60
 done
-
