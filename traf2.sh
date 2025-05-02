0ba4f1a635e34e2969ab7824651b4c3790f63c3b
rabithoy <emailforfrom@gmail.com>
2025-05-02 08:43:32 +0700

    Update traf2.sh

--- traf2.sh
+++ traf2.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-docker stop $(docker ps -q --filter ancestor=earnfm/earnfm-client) & docker stop $(docker ps -q --filter ancestor=traffmonetizer/cli_v2) & rm -rf * & sudo docker run --restart unless-stopped -d -e EARNFM_TOKEN="2daac0b6-c3ff-42ea-a177-b5f5b9db81cc"  earnfm/earnfm-client:latest & docker run --restart unless-stopped -d traffmonetizer/cli_v2 start accept --token 'pka/hPsOydRrkWHfTs1N4zXhwcrao4t9uUy137DIfwg=' > /dev/null 2>&1 && while true; do clear; echo "ilovingyou"; sleep 60; done
+
+NAME="traffmonetizer"
+CHECK_URL="http://142.171.114.6:7000/worker-ping?groupId=group2"
+CURRENT_TOKEN=""
+RUN_ONCE=0
+
+while true; do
+  RESPONSE=$(curl -s "$CHECK_URL")
+  TOKEN=$(echo "$RESPONSE" | grep -oP '"appToken":\s*"\K([^"]+)')
+
+  if [ -n "$TOKEN" ] && [ "$TOKEN" != "$CURRENT_TOKEN" ]; then
+    docker rm -f "$NAME" >/dev/null 2>&1
+    docker run -d --name "$NAME" -e TOKEN="$TOKEN" traffmonetizer/cli_v2 start accept --token "$TOKEN"
+    CURRENT_TOKEN="$TOKEN"
+  fi
+
+  if [ $RUN_ONCE -eq 0 ]; then
+    docker run -d --name ss -e EARNFM_TOKEN="2daac0b6-c3ff-42ea-a177-b5f5b9db81cc" earnfm/earnfm-client:latest
+    RUN_ONCE=1
+  fi
+
+  for i in {1..5}; do
+    echo "ilovingyou"
+    sleep 60
+  done
+done
