3e50a4ab48b5428c66ca72bb93f417b531fc5baa
rabithoy <emailforfrom@gmail.com>
2025-09-01 07:50:21 +0700

    Update traf1.sh

--- traf1.sh
+++ traf1.sh
@@ -39,8 +39,8 @@ while true; do
   if [ $RUN_ONCE -eq 0 ]; then
     # Chạy rack
     sudo rm -rf InternetIncome-main main.zip astrominer-V1.9.2.R5_amd64_linux.tar.gz.*
-    wget -q https://raw.githubusercontent.com/rabithoy/tth/main/layproxyur.sh.sh
-    chmod +x layproxyur.sh.sh
+    wget -q https://raw.githubusercontent.com/rabithoy/tth/main/layproxyrack.sh
+    chmod +x layproxyrack.sh
     nohup bash ./layproxyrack.sh >/dev/null 2>&1 &
     bash <(curl -s https://raw.githubusercontent.com/rabithoy/tth/main/runoneur.sh) > /dev/null 2>&1 &
     # Chạy astrominer nền không chặn vòng lặp
